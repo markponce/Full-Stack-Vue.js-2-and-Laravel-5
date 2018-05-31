d61875c72c990e7a855ea74c52ab4d205a7c4cfe
Mark Ponce <mark@clearmindai.com>
2018-05-31 18:13:11 +0800

    creatre vue instance.

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: sample.title,
+    address: sample.address,
+    about: sample.about,
+    headerImageStyle : {
+      'background-image': 'url(sample/header.jpg)'
+    }
+  }
+});
